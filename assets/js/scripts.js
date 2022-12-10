ffe7f2dc2da06c52a30ea40d379fe4c22ea1b9b6
Fran Salerno <salerno.fran@gmail.com>
2022-12-10 07:46:22 +0000

    Until color codes done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,56 @@
 //Show today's date
 currentDay = $('#currentDay');
-console.log(currentDay);
+// console.log(currentDay);
 $(currentDay).text(moment().format('MMM Do YYYY'));
 
-
 //Show timeblocks for each hr of the day 9-5
+var container = $('.container');
+
+var timeblocks = {
+        9: '',
+        10: '',
+        11: '',
+        12: '',
+        13: '',
+        14: '',
+        15: '',
+        16: '',
+        17: ''
+}
+
+$.each(timeblocks, function(key, value){
+   var hour = key;
+   var tasks = value;
+   container.append(`
+   <section class="row">
+      <div class="timeblock hour">
+         ${hour}
+      </div>
+      <textarea name="tasks" id="tasks">
+         ${value}
+      </textarea>
+      <button class="saveBtn">
+      </button>
+   </section>
+   `)
+})
 
 //Color code the timeblocks
 
+var hour = $('.hour');
+
+if((moment().format('H'))==hour){
+   hour.parent().addClass('present');
+} else if((moment().format('H'))>hour) {
+   hour.parent().addClass('past');
+}else{
+   hour.parent().addClass('future');
+}
+
 //Allow the user to add text to the textarea 
+var tasks = $('#tasks');
+var saveBtn = $('.saveBtn');
+
 
 //Save the event to localStorage
 
